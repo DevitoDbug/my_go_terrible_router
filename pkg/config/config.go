323bc16485284480ea3be43561292da8482d94f9
devitoDbug <davidOchiengy@gmail.com>
2023-12-11 21:31:22 +0300

    Database setup and connection management: Implement database configuration and connection handling.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,3 +1,34 @@
 package config
 
-import _ "github.com/go-sql-driver/mysql"
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+var Db *sql.DB
+
+func GetDBInstance() *sql.DB {
+	return Db
+}
+
+func ConnectToDb() {
+	connectionString := "root:davi@tcp(localhost:3306)/notesAPI_II?charset=utf8&parseTime=True&loc=Local"
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatalf("Error in connecting to db: %v", err)
+		return
+	}
+	Db = db
+}
+func CloseConnectionToDb() {
+	if Db != nil {
+		err := Db.Close()
+		if err != nil {
+			log.Fatalf("Error in closing the db connection: %v", err)
+			return
+		}
+	} else {
+		log.Printf("Db connection was nil")
+	}
+}
