ad782295ba1876d24fd8d7ea5db698aa58cae5ea
devitoDbug <davidOchiengy@gmail.com>
2023-12-11 21:04:37 +0300

    Define controllers: Implement initial versions of controllers for handling todo operations.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module my_go_terrible_router
 
 go 1.21.3
+
+require github.com/go-sql-driver/mysql v1.7.1
--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,6 +1,9 @@
 package routes
 
-import "net/http"
+import (
+	"my_go_terrible_router/pkg/controllers"
+	"net/http"
+)
 
 func Router(w http.ResponseWriter, r *http.Request) {
 	method := r.Method
@@ -10,24 +13,29 @@ func Router(w http.ResponseWriter, r *http.Request) {
 	case "/tasks":
 		//Get all the tasks from db
 		if method == http.MethodGet {
+			controllers.GetAllTasks(w, r)
 			return
 		}
 		//Creating a task
 		if method == http.MethodPost {
+			controllers.CreateTask(w, r)
 			return
 		}
 		http.Error(w, "Bad request", http.StatusBadRequest)
 	case "/tasks/id":
 		//delete a task
 		if method == http.MethodDelete {
+			controllers.DeleteTask(w, r)
 			return
 		}
 		//update a task
 		if method == http.MethodPut {
+			controllers.UpdateTask(w, r)
 			return
 		}
 		//get/read a specific task
 		if method == http.MethodGet {
+			controllers.GetTask(w, r)
 			return
 		}
 		http.Error(w, "Bad request", http.StatusBadRequest)
