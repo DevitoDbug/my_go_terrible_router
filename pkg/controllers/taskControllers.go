f5dfe2dc8599d18f71ed984ae88e6784d3cd3722
devitoDbug <davidOchiengy@gmail.com>
2023-12-12 13:05:57 +0300

    feat: Implement CreateTask handler

--- pkg/controllers/taskControllers.go
+++ pkg/controllers/taskControllers.go
@@ -4,8 +4,39 @@ import (
 	"encoding/json"
 	"log"
 	"my_go_terrible_router/pkg/models"
+	"my_go_terrible_router/pkg/utility"
 	"net/http"
 )
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	task := &models.Task{}
+	// Read the request body
+	err := utility.ReadBody(r, task)
+	if err != nil {
+		log.Printf("Error reading request body: %v", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	//adding the task to db
+	err = task.CreateTask()
+	if err != nil {
+		log.Printf("Error in adding task to db : %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	// Encode the task directly to the response writer
+	enc := json.NewEncoder(w)
+	err = enc.Encode(task)
+	if err != nil {
+		log.Printf("Error encoding task: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+}
 
 func GetAllTasks(w http.ResponseWriter, r *http.Request) {
 	tasks, err := models.GetAllTasks()
@@ -15,27 +46,17 @@ func GetAllTasks(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	res, err := json.Marshal(tasks)
-	if err != nil {
-		log.Printf("Error in marshalling tasks: %v", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
 	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write([]byte(res))
+	enc := json.NewEncoder(w)
+	err = enc.Encode(tasks)
 	if err != nil {
-		log.Printf("Error in response writting: %v", err)
+		log.Printf("Error encoding task: %v", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 }
 
 func GetTask(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func CreateTask(w http.ResponseWriter, r *http.Request) {
 
 }
 func DeleteTask(w http.ResponseWriter, r *http.Request) {
--- pkg/utility/utility.go
+++ pkg/utility/utility.go
@@ -1,1 +1,19 @@
 package utility
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func ReadBody(r *http.Request, task interface{}) error {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(body, task)
+	if err != nil {
+		return err
+	}
+	return nil
+}
